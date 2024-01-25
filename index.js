dfca8bcc83973061cf528fd60db165e2bb092e33
Luiz Herique <127365533+IHeenrique@users.noreply.github.com>
2024-01-25 14:40:05 -0300

    💻 Syncordia API [Portfolio]

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ mg.connect(process.env.mongo_uri, {})
 app.use("/auth", authenticateRouter)
 app.use("/v2", api)
 
-app.get("/cache", async(req, res) => {
-    res.json(authCaches)
-})
-
 app.get("/rbx", async(req, res) => {
     res.redirect("https://authorize.roblox.com/?client_id=4141362268669915168&response_type=code&redirect_uri=https%3A%2F%2Fsyncordia-api-vercel.vercel.app%2Fauth%2Frbx&scope=openid&state=6789&nonce=12345&step=accountConfirm")
 })
